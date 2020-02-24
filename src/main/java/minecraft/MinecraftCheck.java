9d9c0228990589f42af3299c30817d8959adcefe
Paul Häussler <paul.maximilian.haeussler@students.hs-mainz.de>
2020-02-24 21:26:47 +0900

    test jenkins

--- src/main/java/minecraft/MinecraftCheck.java
+++ src/main/java/minecraft/MinecraftCheck.java
@@ -55,7 +55,7 @@ public class MinecraftCheck {
 
 
             tmp = tmp.replaceAll("[^\\p{Print}]", "");
-            serverInfo = tmp.substring(tmp.indexOf("{")).trim();
+            serverInfo = tmp.substring(tmp.indexOf("{")).trim() + "TEST";
             socket.shutdownOutput();
             socket.close();
 
